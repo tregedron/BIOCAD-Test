93866d9641a47431693f6f1873b52a1c6f976782
Mikhail Polovinkin <63188861+tregedron@users.noreply.github.com>
2023-11-27 00:28:42 +0300

    Update run_freee_energy.sh
    added restrains in minimization

--- run_freee_energy.sh
+++ run_freee_energy.sh
@@ -8,12 +8,12 @@ mkdir $MDP
 perl perl_free_energy.pl NPT_free_energy.mdp
 
 # energy minimisation step. -nt - number of cores to run task
-echo "Starting minimisation MD simulation..."
-gmx grompp -f em.mdp -c start.gro -p topol.top -o em.tpr
+echo "Starting energy minimisation..."
+gmx grompp -f em.mdp -c start.gro -p topol.top -r start.gro -o em.tpr
 gmx mdrun -s em.tpr -nt "5" -deffnm em -v
 
 # equilibration step. -nt - number of cores to run task
-echo "Starting equilibration MD simulation..."
+echo "Starting equilibration..."
 gmx grompp -f equil.mdp -c em.gro -p topol.top -o equil.tpr
 gmx mdrun -s equil.tpr -nt "5" -deffnm equil -v
 
