5f0b14fc893fd333e2d24e89a1b7acdc08f6d961
Mikhail Polovinkin <63188861+tregedron@users.noreply.github.com>
2023-11-27 00:21:56 +0300

    Update run_freee_energy.sh
    comments added

--- run_freee_energy.sh
+++ run_freee_energy.sh
@@ -2,27 +2,29 @@
 
 MDP="MDP"
 echo ".mdp files are stored in $MDP"
+# make a new directory to store .mdp files
+mkdir $MDP
+# clone mdp files for all lambda states
+perl perl_free_energy.pl NPT_free_energy.mdp
 
+# energy minimisation step. -nt - number of cores to run task
+echo "Starting minimisation MD simulation..."
 gmx grompp -f em.mdp -c start.gro -p topol.top -o em.tpr
 gmx mdrun -s em.tpr -nt "5" -deffnm em -v
 
+# equilibration step. -nt - number of cores to run task
+echo "Starting equilibration MD simulation..."
 gmx grompp -f equil.mdp -c em.gro -p topol.top -o equil.tpr
 gmx mdrun -s equil.tpr -nt "5" -deffnm equil -v
 
+# Main free energy calculation loop over all lambda states. -nt - number of cores to run task
+echo "Starting free energy calculations via MD simulation..."
 for (( i=0; i<41; i++ ))
 do
     LAMBDA=$i
 
     mkdir Lambda_$LAMBDA
     cd Lambda_$LAMBDA
-
-    # A new directory will be created for each value of lambda and
-    # at each step in the workflow for maximum organization.
-
-    #################
-    # PRODUCTION MD #
-    #################
-    echo "Starting production MD simulation..."
 
     if [ $LAMBDA -eq 0 ]; 
         then
@@ -32,17 +34,13 @@ do
             gmx grompp -f ../$MDP/NPT_free_energy_$LAMBDA.mdp -c ../Lambda_$((i-1))/md$((i-1)).gro -p ../topol.top -o md$LAMBDA.tpr
             gmx mdrun -s md$LAMBDA.tpr -nt "5" -deffnm md$LAMBDA -v; 
         fi
-
-    
-
-    echo "Production MD complete."
-
-    # End
+        
     echo "Ending. Job completed for lambda = $LAMBDA"
 
     cd ../
 done
 
+echo "Finished MD, analysing results..."
 gmx bar -f Lambda_*/md*.xvg -o -oi -b 1000 > result.txt
 
 exit;
